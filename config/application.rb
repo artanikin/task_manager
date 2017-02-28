540f366f09ba1dc08c16adb541b8ce8f8bd7dadf
Anikin Artyom <anikinartyom@gmail.com>
2017-03-01 00:45:12 +0500

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module TaskManager
+  class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        controller_specs: true,
+        view_specs: false,
+        helpers_specs: false,
+        routing_specs: false,
+        requests_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+  end
+end
