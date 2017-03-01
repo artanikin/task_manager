17b28f267147a4481a0abcbabade73a350038419
Anikin Artyom <anikinartyom@gmail.com>
2017-03-02 00:43:44 +0500

    Add default value for role field in User model

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20170301170825) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
-    t.string   "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "role",       default: "user"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
