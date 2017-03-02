ec33ddf0bfd6ba0b5675e60f414d02d11073961c
Anikin Artyom <anikinartyom@gmail.com>
2017-03-03 00:20:54 +0500

    Add faker data for task fatory

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :task do
-    name "MyString"
-    description "MyText"
+    name Faker::Lorem.sentence
+    description Faker::Lorem.paragraph
     user
     state "new"
   end
