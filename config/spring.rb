f417def26a691390e43d257d34f16a10b8933c6a
Anikin Artyom <anikinartyom@gmail.com>
2017-03-06 01:08:38 +0500

    add missing tests

--- config/spring.rb
+++ config/spring.rb
@@ -1,3 +1,12 @@
+if ENV["RAILS_ENV"] == "test"
+  require "simplecov"
+  SimpleCov.start("rails") do
+    add_filter do |source_file|
+      source_file.lines.count < 5
+    end
+  end
+end
+
 %w(
   .ruby-version
   .rbenv-vars
--- spec/controllers/web/sessions_controller_spec.rb
+++ spec/controllers/web/sessions_controller_spec.rb
@@ -21,8 +21,8 @@ RSpec.describe Web::SessionsController, type: :controller do
       expect(session[:user_id]).to eq(user.id)
     end
 
-    it "redirect to root" do
-      expect(response).to redirect_to root_path
+    it "redirect to account tasks" do
+      expect(response).to redirect_to account_tasks_path
     end
 
     context "with invalid attributes" do
@@ -34,6 +34,22 @@ RSpec.describe Web::SessionsController, type: :controller do
 
       it "re-render view sign up form" do
         expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    sign_in_user
+
+    before { delete :destroy }
+
+    context "can sign out" do
+      it "destroy his session" do
+        expect(session[:user_id]).to be_nil
+      end
+
+      it "redirect to root path" do
+        expect(response).to redirect_to root_path
       end
     end
   end
