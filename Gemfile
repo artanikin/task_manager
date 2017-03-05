09de763e2e6cbbb0a5bb00374f30caceae5a0dd3
Anikin Artyom <anikinartyom@gmail.com>
2017-03-05 23:56:50 +0500

    Configure simplecov

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :test do
   gem "rack_session_access"
   gem "fuubar"
   gem "state_machines_rspec"
+  gem "simplecov", require: false
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,14 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "simplecov"
+
+SimpleCov.start "rails" do
+  add_filter do |source_file|
+    source_file.lines.count < 5
+  end
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
